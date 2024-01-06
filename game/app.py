626a88061c83fb82a77e138175aa3232179718b1
Li Junxian <yk690520@outlook.com>
2024-01-06 22:35:18 +0800

    改为独立前端

--- game/app.py
+++ game/app.py
@@ -9,6 +9,8 @@ app = FastAPI()
 
 current_dir=os.path.dirname(__file__)
 static_dir=os.path.join(current_dir, "static")
+assets_dir=os.path.join(current_dir, "static","assets")
 
 app.mount("/static", StaticFiles(directory=static_dir), name="static")
+app.mount("/assets", StaticFiles(directory=assets_dir), name="assets")
 app.include_router(routers.router)
